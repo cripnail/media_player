f147513c9d22c5a28d36b9d052e79fe2963b515e
Nail Sharipov <nikorurk@icloud.com>
2022-10-31 19:36:46 +0500

    Implement ProgressSlider()

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-import '../widgets/video_duration_indicator.dart';
+import '../widgets/progress_slider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -15,8 +15,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late VideoPlayerController _controller;
   late final List<Duration> timestamps;
-  late Animation<double> opacityAnimation;
-  bool showUi = true;
 
   @override
   initState() {
@@ -26,8 +24,7 @@ class _MyHomePageState extends State<MyHomePage> {
       'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
       closedCaptionFile: _loadCaptions(),
     );
-    _controller.addListener(() {
-    });
+    _controller.addListener(() {});
     _controller.setLooping(true);
     _controller.initialize().then((_) => setState(() {}));
     _controller.play();
@@ -41,6 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    const textStyle = TextStyle(color: Colors.white);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -81,35 +79,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         },
                         icon: const Icon(Icons.play_arrow),
                       ),
-                      // const Expanded(
-                      //     child: ProgressSlider(textStyle: textStyle)),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 10,
-              height: 30,
-              width: MediaQuery.of(context).size.width,
-              child: FadeTransition(
-                opacity: opacityAnimation,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5),
-                  child: Column(
-                    children: [
-                      VideoDurationIndicator(controller: _controller),
-                      Expanded(
-                        child: VideoProgressIndicator(
-                          _controller,
-                          allowScrubbing: showUi,
-                          colors: VideoProgressColors(
-                            backgroundColor: Colors.white54,
-                            bufferedColor: Colors.white60,
-                            playedColor: Theme.of(context).colorScheme.secondary,
-                          ),
-                        ),
-                      ),
+                      const Expanded(
+                          child: ProgressSlider(textStyle: textStyle)),
                     ],
                   ),
                 ),
